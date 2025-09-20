39f8a642fba8460915b730e304987a778882b038
Suvi Liimatainen <suviliimatainen@192-168-1-116.lan>
2025-09-20 19:31:49 +0300

    corrected order of coordinates

--- backend/src/main.py
+++ backend/src/main.py
@@ -23,6 +23,6 @@ async def root():
 @app.get("/berlin")
 async def berlin():
     """ returns Berlin coordinates as JSON
-        response format: {"coordinates":[latitude, longitude]}
+        response format: {"coordinates":[longitude, latitude]}
     """
-    return {"coordinates":[52.520008, 13.404954]}
+    return {"coordinates":[13.404954, 52.520008]}
