419254dba8cdcc863d6903a894373cf3fe682b8e
susulmtnn <63512357+susulmtnn@users.noreply.github.com>
2025-10-02 20:20:00 +0300

    Feature/unittests implemented using jest (#103)
    * feat: implemented jest tests for MapComponent #18
    
    * feat: enabled invoking frontend tests in tasks.py
    
    * feat: add .env update and invoke tasks for tests #18
    
    * fix : renamed .env file correctly so git ignores it
    
    * Delete backend/tests/unit/test_example 2.py
    
    * fix : Delete backend/tests/unit/__init__ 2.py
    
    * feat: added coverage to invoke tasks #18
    
    * fix : Delete frontend/.env.example
    
    * feat : Backend and frontend tests run with invoke
    
    * fix : Remove frontend coverage folder from Git #18
    
    * feat: coverage report file structure, edited gitignore #70
    
    ---------
    
    Co-authored-by: Suvi Liimatainen <suviliimatainen@Suvis-MacBook-Air.local>
    Co-authored-by: Suvi Liimatainen <suviliimatainen@192-168-1-116.lan>

--- frontend/src/components/MapComponent.tsx
+++ frontend/src/components/MapComponent.tsx
@@ -19,8 +19,8 @@ interface MapComponentProps {
 
 const MapComponent: React.FC<MapComponentProps> = ({fromLocked, toLocked}) => {
 
-  const mapboxToken = process.env.REACT_APP_MAPBOX_TOKEN || 'Mapbox token is needed in order to use the map';
-  const mapboxStyle = process.env.REACT_APP_MAPBOX_STYLE || 'Mapbox style is needed in order to use the map';
+  const mapboxToken = process.env.REACT_APP_MAPBOX_TOKEN || '';
+  const mapboxStyle = process.env.REACT_APP_MAPBOX_STYLE || '';
   const mapboxRef = useRef<HTMLDivElement>(null);
   const mapRef = useRef<mapboxgl.Map | null>(null)
   const fromMarkerRef = useRef<mapboxgl.Marker | null>(null);
@@ -70,14 +70,16 @@ const MapComponent: React.FC<MapComponentProps> = ({fromLocked, toLocked}) => {
   if (mapboxToken) {
     return (
       <div style={{ height: "100vh", width: "100%" }}>
-        <div ref={mapboxRef} style={{ height: "100%", width: "100%" }} />
+        
+        <div ref={mapboxRef} 
+        data-testid="mapbox-map" 
+        style={{ height: "100%", width: "100%" }} />
       </div>
     );
   }
  
-
-
-  return (
+  if (!mapboxToken){
+    return (
     <div style={{ height: "100vh", width: "100%" }}>
       <MapContainer
         center={berlinCenter}
@@ -90,7 +92,13 @@ const MapComponent: React.FC<MapComponentProps> = ({fromLocked, toLocked}) => {
         />
       </MapContainer>
     </div>
+  
   );
-};
+}
+/* We ignore this line in coverage report, because it is unreachable.
+However, typescript requires handling this corner case */
+//istanbul ignore next
+return null;
+}
 
 export default MapComponent;
