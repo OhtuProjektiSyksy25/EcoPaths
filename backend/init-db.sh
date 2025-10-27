219293ef0321b24fc2bf509d048bfeb834274eb1
l-antti <laura.k.anttila@gmail.com>
2025-10-27 19:22:43 +0200

    fix: rerun workflow #155

--- backend/init-db.sh
+++ backend/init-db.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 
-# Locate the pg_hba.conf file and append a trust rule for TCP connections
+# Locate the pg_hba.conf file and append a trust rule for TCP connection
 PG_HBA=$(find /etc/postgresql -name pg_hba.conf | head -n 1)
 
 echo "Adding trust rule to file: $PG_HBA"
