7b4bf0ea9cb3de87faa7a6a3cab9cd835e5c41c3
eerojantunen <jant.eero@gmail.com>
2025-10-28 01:10:57 +0200

    added test for _get_tile_edges

--- backend/tests/unit/services/test_route_service.py
+++ backend/tests/unit/services/test_route_service.py
@@ -2,6 +2,7 @@
 import pytest
 import geopandas as gpd
 from shapely.geometry import LineString, Point
+from unittest.mock import patch, MagicMock
 from src.services.route_service import RouteService
 
 
@@ -65,3 +66,45 @@ def test_route_service_has_required_columns(sample_edges):
     required_columns = ["geometry", "edge_id", "length_m", "aqi", "tile_id"]
     for col in required_columns:
         assert col in service.edges.columns
+
+
+def test_get_tile_edges(sample_edges):
+    """
+        Test that _get_tile_edges combines data from existing and non existing tiles correctly
+        and returns correct GeoDataFrame
+        Redis and enrichment are mocked
+    """
+    redis_mock = MagicMock()
+    route_service = RouteService(edges=sample_edges, redis=redis_mock)
+    tile_ids = ["r1_c1", "r1_c1", "r2_c2"]
+
+    existing_gdf = sample_edges[sample_edges["tile_id"] == "r1_c1"]
+    new_gdf = gpd.GeoDataFrame({
+        "geometry": [LineString([(3, 3), (4, 4)])],
+        "edge_id": ["e4"],
+        "length_m": [141.4],
+        "aqi": [15],
+        "tile_id": ["r2_c2"]
+    }, crs="EPSG:3067")
+
+    with patch("services.route_service.RedisService.prune_found_ids",
+                return_value=["r2_c2"]) as mock_prune, \
+         patch("services.route_service.RedisService.get_gdf_by_list_of_keys") as mock_get_gdf, \
+         patch("services.route_service.RedisService.edge_enricher_to_redis_handler") as mock_enrich:
+
+        mock_get_gdf.side_effect = [
+            (existing_gdf, []),
+            (new_gdf, [])
+        ]
+        mock_enrich.return_value = True
+
+        result = route_service._get_tile_edges(tile_ids)
+
+        assert isinstance(result, gpd.GeoDataFrame)
+        print(result["tile_id"])
+        assert set(result["tile_id"]) == {"r1_c1", "r1_c1", "r2_c2"}
+        assert set(result["edge_id"]) == {"e1", "e2", "e4"}
+
+        mock_prune.assert_called_once_with(tile_ids, redis_mock)
+        mock_enrich.assert_called_once_with(["r2_c2"], redis_mock)
+        assert mock_get_gdf.call_count == 2
