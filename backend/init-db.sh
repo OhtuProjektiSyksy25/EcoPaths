7984527e05de2e6d8c54d0285f1271fbfa98b6de
l-antti <laura.k.anttila@gmail.com>
2025-10-27 19:19:03 +0200

    fix: modify init-db script #155

--- backend/init-db.sh
+++ backend/init-db.sh
@@ -1,2 +1,11 @@
 #!/bin/bash
-echo "host all all all trust" >> /etc/postgresql/pg_hba.conf
+set -e
+
+# Locate the pg_hba.conf file and append a trust rule for TCP connections
+PG_HBA=$(find /etc/postgresql -name pg_hba.conf | head -n 1)
+
+echo "Adding trust rule to file: $PG_HBA"
+echo "host all all all trust" >> "$PG_HBA"
+
+
+
