9eb814143b5136a94a7db05e884ff6ebe48b3bf7
Juho Kronlöf <juho.kronlof@gmail.com>
2025-12-09 14:25:47 +0200

    configure area dropdown button color and add buttons for clearing input boxes

--- backend/src/app.py
+++ backend/src/app.py
@@ -27,6 +27,8 @@ ALLOWED_ORIGINS = [
     "http://0.0.0.0:8000",
     "https://photon.komoot.io",
     "http://172.25.211.59:3000",
+    "http://192.168.1.162:3000",
+    "http://192.168.1.162:8000",
 ]
 
 if TEST_MODE:
